45f0ffccdd971cd8e4a11d9aad53b936486baebc
Oliver Belmont <obelmont@oliverbelmont.com>
2018-02-12 15:55:54 +1100

    new directives
    Signed-off-by: Oliver Belmont <obelmont@oliverbelmont.com>

--- app/setup.php
+++ app/setup.php
@@ -125,4 +125,276 @@ add_action('after_setup_theme', function () {
     sage('blade')->compiler()->directive('asset', function ($asset) {
         return "<?= " . __NAMESPACE__ . "\\asset_path({$asset}); ?>";
     });
+
+    /**
+     * Advanced Custom Fields Blade directives
+     */
+
+    /**
+     * Create @getLayout() Blade directive
+     */
+    sage('blade')->compiler()->directive('ifLayout', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = "<?php if( get_row_layout() == $expression ): ?>";
+        return $output;
+    });
+
+    /**
+     * Create @elseLayout() Blade directive
+     */
+    sage('blade')->compiler()->directive('elseLayout', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = "<?php elseif( get_row_layout() == $expression ): ?>";
+        return $output;
+    });
+
+    /**
+     * Create @endLayout Blade directive
+     */
+    sage('blade')->compiler()->directive('endLayout', function () {
+        return "<?php endif; ?>";
+    });
+
+
+    /**
+     * Create @fields() Blade directive
+     */
+    sage('blade')->compiler()->directive('fields', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = "<?php if (have_rows($expression)) : ?>";
+        $output .= "<?php while (have_rows($expression)) : ?>";
+        $output .= "<?php the_row(); ?>";
+        return $output;
+    });
+
+    /**
+     * Create @endFields Blade directive
+     */
+    sage('blade')->compiler()->directive('endFields', function () {
+        return "<?php endwhile; endif; ?>";
+    });
+
+    /**
+     * Create @field() Blade directive
+     */
+    sage('blade')->compiler()->directive('field', function ($expression) {
+         $expression = strtr($expression, array('(' => '', ')' => ''));
+         return "<?php the_field($expression); ?>";
+    });
+
+    /**
+     * Create @getField() Blade directive
+     */
+    sage('blade')->compiler()->directive('getField', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        return "<?php get_field($expression); ?>";
+    });
+
+    /**
+     * Create @hasField() Blade directive
+     */
+    sage('blade')->compiler()->directive('hasField', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        return "<?php if (get_field($expression)) : ?>";
+    });
+
+    /**
+     * Create @endField Blade directive
+     */
+    sage('blade')->compiler()->directive('endField', function () {
+        return "<?php endif; ?>";
+    });
+
+    /**
+     * Create @sub() Blade directive
+     */
+    sage('blade')->compiler()->directive('sub', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        return "<?php the_sub_field($expression); ?>";
+    });
+
+    /**
+     * Create @getSub() Blade directive
+     */
+    sage('blade')->compiler()->directive('getSub', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        return "<?php get_sub_field($expression); ?>";
+    });
+
+    /**
+     * Create @hasSub() Blade directive
+     */
+    sage('blade')->compiler()->directive('hasSub', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        return "<?php if (get_sub_field($expression)) : ?>";
+    });
+
+    /**
+     * Create @endSub Blade directive
+     */
+    sage('blade')->compiler()->directive('endSub', function () {
+        return "<?php endif; ?>";
+    });
+
+    /**
+     * Create @img() Blade directives
+     */
+    sage('blade')->compiler()->directive('recImg', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = '<div class="ratio--rec">';
+        $output .= '<img class="lazyload" data-sizes="auto" data-src="';
+        $output .= "<?php echo get_field($expression)['sizes']['rec-medium']; ?>";
+        $output .= '" data-srcset="';
+        $output .= "<?php echo get_field($expression)['sizes']['rec-small']; ?> 800w,";
+        $output .= "<?php echo get_field($expression)['sizes']['rec-medium']; ?> 1600w,";
+        $output .= "<?php echo get_field($expression)['sizes']['rec-large']; ?> 1920w,";
+        $output .= '" />';
+        $output .= '</div>';
+        return $output;
+    });
+
+    sage('blade')->compiler()->directive('squImg', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = '<div class="ratio--squ">';
+        $output .= '<img class="lazyload" data-sizes="(min-width: 768px) 50vw, 100vw" data-src="';
+        $output .= "<?php echo get_field($expression)['sizes']['squ-medium']; ?>";
+        $output .= '" data-srcset="';
+        $output .= "<?php echo get_field($expression)['sizes']['squ-small']; ?> 800w,";
+        $output .= "<?php echo get_field($expression)['sizes']['squ-medium']; ?> 1600w,";
+        $output .= "<?php echo get_field($expression)['sizes']['squ-large']; ?> 1920w,";
+        $output .= '" />';
+        $output .= '</div>';
+        return $output;
+    });
+
+    sage('blade')->compiler()->directive('widImg', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = '<div class="ratio--wid">';
+        $output .= '<img class="lazyload" data-sizes="auto" data-src="';
+        $output .= "<?php echo get_field($expression)['sizes']['wid-medium']; ?>";
+        $output .= '" data-srcset="';
+        $output .= "<?php echo get_field($expression)['sizes']['wid-small']; ?> 800w,";
+        $output .= "<?php echo get_field($expression)['sizes']['wid-medium']; ?> 1600w,";
+        $output .= "<?php echo get_field($expression)['sizes']['wid-large']; ?> 1920w,";
+        $output .= '" />';
+        $output .= '</div>';
+        return $output;
+    });
+
+    sage('blade')->compiler()->directive('recSub', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = '<div class="ratio--rec">';
+        $output .= '<img class="lazyload" data-sizes="auto" data-src="';
+        $output .= "<?php echo get_field($expression)['sizes']['rec-medium']; ?>";
+        $output .= '" data-srcset="';
+        $output .= "<?php echo get_sub_field($expression)['sizes']['rec-small']; ?> 800w,";
+        $output .= "<?php echo get_sub_field($expression)['sizes']['rec-medium']; ?> 1600w,";
+        $output .= "<?php echo get_sub_field($expression)['sizes']['rec-large']; ?> 1920w,";
+        $output .= '" />';
+        $output .= '</div>';
+        return $output;
+    });
+
+    sage('blade')->compiler()->directive('squSub', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = '<div class="ratio--squ">';
+        $output .= '<img class="lazyload" data-sizes="(min-width: 768px) 50vw, 100vw" data-src="';
+        $output .= "<?php echo get_sub_field($expression)['sizes']['squ-medium']; ?>";
+        $output .= '" data-srcset="';
+        $output .= "<?php echo get_sub_field($expression)['sizes']['squ-small']; ?> 800w,";
+        $output .= "<?php echo get_sub_field($expression)['sizes']['squ-medium']; ?> 1600w,";
+        $output .= "<?php echo get_sub_field($expression)['sizes']['squ-large']; ?> 1920w,";
+        $output .= '" />';
+        $output .= '</div>';
+        return $output;
+    });
+
+    sage('blade')->compiler()->directive('widSub', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = '<div class="ratio--wid">';
+        $output .= '<img class="lazyload" data-sizes="auto" data-src="';
+        $output .= "<?php echo get_sub_field($expression)['sizes']['wid-medium']; ?>";
+        $output .= '" data-srcset="';
+        $output .= "<?php echo get_sub_field($expression)['sizes']['wid-small']; ?> 800w,";
+        $output .= "<?php echo get_sub_field($expression)['sizes']['wid-medium']; ?> 1600w,";
+        $output .= "<?php echo get_sub_field($expression)['sizes']['wid-large']; ?> 1920w,";
+        $output .= '" />';
+        $output .= '</div>';
+        return $output;
+    });
+
+    sage('blade')->compiler()->directive('miniSub', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = '<div class="ratio--rec ratio--mini">';
+        $output .= '<img class="lazyload" data-src="';
+        $output .= "<?php echo get_sub_field($expression)['sizes']['rec-small']; ?>";
+        $output .= '">';
+        $output .= '</div>';
+        return $output;
+    });
+
+    sage('blade')->compiler()->directive('thumbImg', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = '<div class="ratio--squ">';
+        $output .= '<img class="lazyload" data-src="';
+        $output .= "<?php echo get_field($expression)['sizes']['squ-small']; ?>";
+        $output .= '">';
+        $output .= '</div>';
+        return $output;
+    });
+
+    sage('blade')->compiler()->directive('thumbSub', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = '<div class="ratio--squ">';
+        $output .= '<img class="lazyload" data-src="';
+        $output .= "<?php echo get_sub_field($expression)['sizes']['squ-small']; ?>";
+        $output .= '">';
+        $output .= '</div>';
+        return $output;
+    });
+
+    sage('blade')->compiler()->directive('squTax', function ($expression) {
+        $term = get_queried_object();
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = '<div class="ratio--squ">';
+        $output .= '<img class="lazyload" data-sizes="(min-width: 768px) 50vw, 100vw" data-src="';
+        $output .= "<?php echo get_field($expression, $term)['sizes']['squ-medium']; ?>";
+        $output .= '" data-srcset="';
+        $output .= "<?php echo get_field($expression, $term)['sizes']['squ-small']; ?> 800w,";
+        $output .= "<?php echo get_field($expression, $term)['sizes']['squ-medium']; ?> 1600w,";
+        $output .= "<?php echo get_field($expression, $term)['sizes']['squ-large']; ?> 1920w,";
+        $output .= '" />';
+        $output .= '</div>';
+        return $output;
+    });
+
+    /**
+     * Create @theGallery() Blade directive
+     */
+    sage('blade')->compiler()->directive('theGallery', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = "<?php if(get_field($expression)): ?>";
+        $output .= "<?php foreach( get_field($expression) ";
+        $output .= 'as $image ):?>';
+        $output .= '<div class="ratio--rec">';
+        $output .= '<img class="lazyload" data-sizes="auto" data-src="';
+        $output .= '<?php echo $image["sizes"]["product-image"]; ?>';
+        $output .= '" data-srcset="';
+        $output .= '<?php echo $image["sizes"]["rec-small"]; ?> 800w,';
+        $output .= '<?php echo $image["sizes"]["product-image"]; ?> 1000w,';
+        $output .= '" />';
+        $output .= '</div>';
+        $output .= '<?php endforeach; ?>';
+        $output .= '<?php endif; ?>';
+        return $output;
+    });
+    // Started creating a 'trans' directive, not done yet
+    sage('blade')->compiler()->directive('trans', function ($expression) {
+        $expression = strtr($expression, array('(' => '', ')' => ''));
+        $output = "<?php if( have_rows('trans_terms' , 'option') ):";
+        $output .= "while( have_rows('trans_terms', 'option') ): the_row(); ?>";
+        $output .= "<?php echo the_sub_field($expression); ?>";
+        $output .= "<?php endwhile; endif; ?>";
+        return $output;
+    });
 });
