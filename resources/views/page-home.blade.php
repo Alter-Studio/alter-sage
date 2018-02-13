df73624b2859708561ccbe978a44fa245c3cdac3
Oliver Belmont <obelmont@oliverbelmont.com>
2018-02-13 14:01:15 +1100

    Basic Image
    Signed-off-by: Oliver Belmont <obelmont@oliverbelmont.com>

--- resources/views/page-home.blade.php
+++ resources/views/page-home.blade.php
@@ -4,5 +4,20 @@
 @section('content')
   <div class="container">
 	{{ get_the_title() }}
+		<div style="max-width: 100%">
+			<div class="inner">
+
+				{{-- Basic Responsive Image | One Ratio --}}
+				<div class="img--responsive" style="padding-bottom: {{ get_field('image')['sizes']['large-ratio'] }}%" >
+					<img class="lazyload" 
+						data-sizes="auto" 
+						data-src="
+							{{ get_field('image')['sizes']['large'] }}
+						"
+					/>
+				</div>
+
+			</div>
+		</div>
   </div>
 @endsection
