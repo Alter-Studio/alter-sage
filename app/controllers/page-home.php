40ee1acb4ca19d7fcf806cd6be0469d0142c4264
Oliver Belmont <obelmont@oliverbelmont.com>
2018-02-13 16:14:20 +1100

    Controller updates
    Signed-off-by: Oliver Belmont <obelmont@oliverbelmont.com>

---
+++ app/controllers/page-home.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Sober\Controller\Controller;
+
+class PageHome extends Controller
+{
+	// Pass on all fields from Advanced Custom Fields to the view
+    protected $acf = true;
+}
