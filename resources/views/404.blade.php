c8758e8b6555c29cce311ae4c5b583cf7396d49d
Oliver Belmont <obelmont@oliverbelmont.com>
2018-02-13 11:48:38 +1100

    header and footer partials
    Signed-off-by: Oliver Belmont <obelmont@oliverbelmont.com>

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -5,8 +5,7 @@
 	<div class="error">
 	  <div class="container">
 		<div class="error-message">
-			<p class="large">@trans('not_found_message')</p>
-			<a class="zoom left large" href="{{ home_url() }}">@trans('not_found_link_text')</a>
+			<a class="zoom left large" href="{{ home_url() }}">404 Error</a>
 		</div>
 	  </div>
 	</div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header class="desktop-header">
+<header class="header">
     <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
