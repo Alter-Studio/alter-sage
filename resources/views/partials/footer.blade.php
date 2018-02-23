2127355dfa3d81b17f5d4d103907ec7135d5ccfb
Oliver Belmont <obelmont@oliverbelmont.com>
2018-02-23 13:37:09 +1100

    Analytics update
    Signed-off-by: Oliver Belmont <obelmont@oliverbelmont.com>

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,13 @@
 <footer class="footer">
 	{{-- footer container --}}
 </div>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=TRACKING_CODE"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'TRACKING_CODE');
+</script>    
